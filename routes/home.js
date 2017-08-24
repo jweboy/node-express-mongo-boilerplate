ad395bf452318339bc990aa3aa78e3a2f35fe0b2
jweboy <1259524459@qq.com>
2017-08-24 15:15:48 +0800

    add mongodb model

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const { User } = require('../lib/mongo')
 
 router.get('/', function (req, res) {
   const { username, password } = req.session
+
+  User.insertOne({ username, password }).exec().then(console.log).catch(console.error)
+
   res.render('home', {
     name: username,
     pwd: password
